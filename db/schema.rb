917ec058ab0337688fedea113a2782ae26e78973
tonnybrito <tonny@devlandia.net>
2020-06-01 11:05:21 -0300

    meditarte changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180912141003) do
+ActiveRecord::Schema.define(version: 20200529150853) do
+
+  create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "country"
+    t.string "city"
+    t.string "state"
+    t.string "neighborhood"
+    t.string "street"
+    t.string "number"
+    t.bigint "client_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_addresses_on_client_id"
+  end
 
   create_table "clients", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "nome"
@@ -31,6 +45,7 @@ ActiveRecord::Schema.define(version: 20180912141003) do
     t.string "situação"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -45,4 +60,5 @@ ActiveRecord::Schema.define(version: 20180912141003) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "clients"
 end
